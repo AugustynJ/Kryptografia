9b4a364503565e01b8c23f16299fa788426966d7
Arek <92780875+GetArektt@users.noreply.github.com>
2023-01-03 10:11:42 +0100

    sign working

--- STS.py
+++ STS.py
@@ -2,24 +2,27 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP, PKCS1_v1_5
 from Crypto.Hash import SHA256
 from Crypto.Random import random
-import base64
-import hashlib
+from Crypto.Signature import PKCS1_v1_5
+from base64 import b64encode, b64decode
 
 
-def sign(message, priv_key):
+def sign_message(message, priv_key):
     key = RSA.importKey(priv_key)
-    # hash = SHA256.new(message)
-    h = hashlib.new('sha256')
-    h.update(bytes(str(message), 'utf-8'))
-    # hash = sha256(message_encoded)
-    # to_return = PKCS1_OAEP.new(key)
-    return sign(h, priv_key).encode("base64")
+    digest = SHA256.new()
+    digest.update(message.encode('utf-8'))
+    signer = PKCS1_v1_5.new(key)
+    sig = signer.sign(digest)
+    return sig.hex()
 
 
 def verify(message, signature, pub_key):
     key = RSA.importKey(pub_key)
-    hash = SHA256.new(message)
-    if PKCS1_OAEP.new(key).verify(hash, base64.b64decode(signature)):
+    sig = bytes.fromhex(message)
+    digest = SHA256.new()
+    digest.update(message.encode('utf-8'))
+    verifier = PKCS1_v1_5.new(public_key)
+    verified = verifier.verify(digest, sig)
+    if verified:
         return True
     else:
         print("Weryfikacja się nie powiodła")
@@ -39,7 +42,7 @@ def decrypt(message, priv_key):
 # https: // the-it-ninja.blogspot.com/2015/11/station-to-station-encryption-in -python.html
 
 # Ustalenie losowej stałej
-g = 4200
+g = 4
 
 # Utworzenie klucza publicznego i prywatnego Alice
 key_1 = RSA.generate(1024)
@@ -55,19 +58,21 @@ B_pub_key = key_2.publickey().exportKey('PEM')
 # Alice wybiera liczbe x,liczy g^x i przesyła Bobowi
 x = 10
 g_x = g ** x
-print(f"Alice wybiera liczbe x {x},liczy g^x {g_x} i przesyła Bobowi")
+print(f"Alice wybiera liczbe x={x}\nliczy g^x={g_x} \ni przesyła Bobowi\n")
 
 # Bob wybiera liczbe y,liczy g^y
 y = 25
 g_xy = g_x ** y
 
-print(g_xy)
 print(
-    f"Bob wybiera liczbe y {y},liczy (g^x)^y {g_xy}, następnie podpisuje ten składnik swoim kluczem prywatnym i przesyła Alice")
-print(B_priv_key)
-g_xy_signed = sign(str(g_xy), B_priv_key)
+    f"Bob wybiera liczbe y={y}\nliczy (g^x)^y={g_xy}\nnastępnie podpisuje ten składnik swoim kluczem prywatnym i przesyła Alice\n")
+# podpisany hash w postaci szestnastkowej
+g_xy_signed = sign_message(str(g_xy), B_priv_key)
+
+print(g_xy_signed)
 
 # Następnie Alice dostaje podpisaną wiadomość przez Boba, sprawdza jej poprawność i oblicza x Boba
-x_alice = g_xy**(1/y)
+x_alice = pow(g_xy, (1/y))
 if verify(x_alice, g_xy_signed, B_pub_key):
     pass
+    print("Udana weryfikacja")
